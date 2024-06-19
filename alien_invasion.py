ef56620dd5ae6f82100308dec0ba38eae808f1cd
Alejandro Alamillo <aalamillom92@gmail.com>
2024-06-19 23:56:04 +0200

    creat helper class _fire_bullet() and update _check_keydown_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -49,6 +49,9 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
 
     def _check_keyup_events(self, event):
         """Respond to key releases"""
@@ -57,16 +60,22 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
-
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
 
         #Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
             
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
 if __name__ == '__main__':
     # Make a game instance and run the game.
