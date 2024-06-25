1113a6a3e28fd11014b49f7588962d4e9711fb6e
Alejandro Alamillo <aalamillom92@gmail.com>
2024-06-25 15:48:43 +0200

    add Alien settings

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
-        #Screen settings.
+        # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
@@ -17,3 +17,6 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
