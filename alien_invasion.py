b75ea561027e7047f8036fb05f3b23026f71b54b
Alejandro Alamillo <aalamillom92@gmail.com>
2024-06-25 17:38:03 +0200

    create _check_fleet_edges() _change_fleet_direction() and update _update_aliens()

--- alien_invasion.py
+++ alien_invasion.py
@@ -50,6 +50,19 @@ class AlienInvasion:
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
+    def _check_fleet_edges(self):
+        """Respond appropriately if any alien have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -100,7 +113,8 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
     def _update_aliens(self):
-        """Update the position of all aliens in the fleet."""
+        """Check if the fleet is at an edge, then update positions."""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _update_screen(self):
