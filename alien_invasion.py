927cbb094ed48eb0e2e603441cf9674207e51467
Alejandro Alamillo <aalamillom92@gmail.com>
2024-06-26 17:51:33 +0200

    add time and game_stats import and update __init__ to create a GameStats instance.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,10 @@
 import sys
+from time import sleep
+
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -18,6 +21,10 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
                     (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
