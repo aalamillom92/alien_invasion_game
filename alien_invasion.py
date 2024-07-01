335eb0e710d41e520021754ef2ae16e7333ae65f
Alejandro Alamillo <aalamillom92@gmail.com>
2024-07-01 16:37:59 +0200

    create helper method _check_play_button() and update check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -34,6 +35,10 @@ class AlienInvasion:
 
         # Start Alien Invasion in an active state.
         self.game_active = False
+
+        # Make the Play button.
+        if not self.game_active:
+            self.play_button = Button(self, "Play")
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
@@ -96,6 +101,14 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.game_active = True
 
     def _check_keydown_events(self, event):
         """Respond to key presses."""
@@ -187,6 +200,10 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive.
+        if not self.game_active:
+            self.play_button.draw_button()
             
         # Make the most recently drawn screen visible.
         pygame.display.flip()
