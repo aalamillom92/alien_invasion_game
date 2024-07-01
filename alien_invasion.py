59113c457a6ad298493b10eaf342af28c468992d
Alejandro Alamillo <aalamillom92@gmail.com>
2024-07-01 17:11:59 +0200

    update _ship_hit to make cursor reappear after a new game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -121,6 +121,9 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
+
     def _check_keydown_events(self, event):
         """Respond to key presses."""
         if event.key == pygame.K_RIGHT:
@@ -193,6 +196,7 @@ class AlienInvasion:
 
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """Check if any alien have reached the bottom off the screen"""
