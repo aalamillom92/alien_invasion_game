93d59c11dd66a8c0d021091f44262b222373b8eb
Alejandro Alamillo <aalamillom92@gmail.com>
2024-07-04 23:20:26 +0200

    update show_score() to show ships on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -116,6 +116,8 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
             self.game_active = True
 
             # Get rid of any remainign bullets and aliens.
@@ -196,8 +198,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an Alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
